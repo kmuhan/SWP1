c15e3c2ec61a04b6511f76d184723d44e3625c4b
kmu-han <hkjung1123@gmail.com>
2020-06-27 06:19:56 +0900

    Issue #4

---
+++ calc/assignment_template.py
@@ -0,0 +1,16 @@
+html = b"""
+<html>
+	<body>
+		<form action="">
+			A: <input type="number" name="a">
+			B: <input type="number" name="b">
+			<input type="submit">
+		</form>
+		<p>
+			sum: %(sum)s</br>
+			mul: %(mul)s</br>
+		<p/>
+	</body>
+</html>
+"""
+			
---
+++ calc/compute.py
@@ -0,0 +1,25 @@
+from cgi import parse_qs
+from assignment_template import html
+
+def application(environ, start_response) :
+	d = parse_qs(environ['QUERY_STRING'])
+	a = d.get('a', [''])[0]
+	b = d.get('b', [''])[0]
+	sum = 0
+	mul = 0
+	if '' not in [a, b]:
+		a, b = int(a), int(b)
+		sum = [a+b]
+		mul = [a*b]
+	response_body = html % {
+		'sum' : sum,
+		'mul' : mul,
+	}
+
+	start_response('200 OK', [
+		('Content-Type', 'text/html'),
+		('Content-Length', str(len(response_body)))
+	])
+	return [response_body]
+		
+
