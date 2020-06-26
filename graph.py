3187e4ae0ae4b2cadc14f645b9f50f7cfbe94d20
kmu-han <hkjung1123@gmail.com>
2020-06-26 22:58:44 +0900

    Modified image path

--- graph.py
+++ graph.py
@@ -26,7 +26,7 @@ def application(environ, start_response):
             fig = plt.figure()
             graph = plt.plot(x, y)
             plt.grid()
-            fig.savefig('graph.png')
+            fig.savefig('img/graph.png')
         response_body = html
         start_response('200 OK', [
             ('Content-Type', 'text/html'),
